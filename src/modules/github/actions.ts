3870b70a288616ff859c74436e35bf30566c61c8
hychoi <hayeong28@naver.com>
2020-07-26 18:02:46 +0900

    fix: createAsyncAction

--- src/modules/github/actions.ts
+++ src/modules/github/actions.ts
@@ -1,4 +1,4 @@
-import { action, createAction } from "typesafe-actions";
+import { createAsyncAction } from "typesafe-actions";
 import { GithubProfile } from "../../api/github";
 import { AxiosError } from "axios";
 
@@ -6,12 +6,18 @@ export const GET_USER_PROFILE = "github/GET_USER_PROFILE";
 export const GET_USER_PROFILE_SUCCESS = "github/GET_USER_PROFILE_SUCCESS";
 export const GET_USER_PROFILE_ERROR = "github/GET_USER_PROFILE_ERROR";
 
-export const getUserProfile = action(GET_USER_PROFILE);
-export const getUserProfileSuccess = createAction(
+// export const getUserProfile = action(GET_USER_PROFILE);
+// export const getUserProfileSuccess = createAction(
+//   GET_USER_PROFILE_SUCCESS,
+//   (action) => (payload: GithubProfile) => payload
+// )();
+// export const getUserProfileError = createAction(
+//   GET_USER_PROFILE_ERROR,
+//   (action) => (payload: AxiosError) => payload
+// )();
+
+export const getUserProfileAsync = createAsyncAction(
+  GET_USER_PROFILE,
   GET_USER_PROFILE_SUCCESS,
-  (action) => (payload: GithubProfile) => payload
-)();
-export const getUserProfileError = createAction(
-  GET_USER_PROFILE_ERROR,
-  (action) => (payload: AxiosError) => payload
-)();
+  GET_USER_PROFILE_ERROR
+)<undefined, GithubProfile, AxiosError>();
