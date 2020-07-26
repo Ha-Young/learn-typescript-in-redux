fe9f0e95a2e8c6ca0979c6d48e9670d79973d54e
Ha0 <hayeong28@naver.com>
2020-07-26 23:55:44 +0900

    add: github container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import CounterContainer from "./container/CounterContainer";
+import GithubProfileLoader from "./container/GithubProfileLoader";
 
 function App() {
   return (
     <div className="App">
-      <CounterContainer />
+      <GithubProfileLoader />
     </div>
   );
 }
---
+++ src/container/GithubProfileLoader.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../modules";
+import { getUserProfileThunk } from "../modules/github";
+import GithubUsernameForm from "../components/GithubUsernameForm";
+import GithubProfileInfo from "../components/GithubProfileInfo";
+
+function GithubProfileLoader() {
+  const { data, loading, error } = useSelector(
+    (state: RootState) => state.github.userProfile
+  );
+  const dispatch = useDispatch();
+
+  const onSubmitUsername = (username: string) => {
+    dispatch(getUserProfileThunk(username));
+  };
+
+  return (
+    <>
+      <GithubUsernameForm onSubmitUsername={onSubmitUsername} />
+      {loading && <p style={{ textAlign: "center" }}>로딩중..</p>}
+      {error && <p style={{ textAlign: "center" }}>애러발생!</p>}
+      {data && (
+        <GithubProfileInfo
+          bio={data.bio}
+          blog={data.blog}
+          name={data.name}
+          thumbnail={data.avatar_url}
+        />
+      )}
+    </>
+  );
+}
+
+export default GithubProfileLoader;
