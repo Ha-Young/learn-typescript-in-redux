9620623765099c1a75d25a0551dd43536ea33d75
Ha0 <hayeong28@naver.com>
2020-07-26 23:24:28 +0900

    add: regist github redux made by tunk to rootReducer

--- src/modules/github/actions.ts
+++ src/modules/github/actions.ts
@@ -5,6 +5,7 @@ import { AxiosError } from "axios";
 export const GET_USER_PROFILE = "github/GET_USER_PROFILE";
 export const GET_USER_PROFILE_SUCCESS = "github/GET_USER_PROFILE_SUCCESS";
 export const GET_USER_PROFILE_ERROR = "github/GET_USER_PROFILE_ERROR";
+export const GET_GET_GET = "github/GET_GET_GET";
 
 export const getUserProfile = createAction(GET_USER_PROFILE)();
 export const getUserProfileSuccess = createAction(
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import counter from "./counter";
+import todos from "./todos";
+import github from "./github";
 
 const rootReducer = combineReducers({
   counter,
+  todos,
+  github,
 });
 
 export default rootReducer;
